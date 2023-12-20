34f36c420f2b90a7fac13c318a5fc8541aa09b30
kavinduharshamal <122147919+kavinduharshamal@users.noreply.github.com>
2023-12-20 19:36:33 +0530

    add loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Suspense } from "react";
+import { Loader } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 10], fov: 50 }}>
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [0, 0, 10], fov: 50 }}>
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
